ba2773c96fcfb849efed74effd04a2c7fdde1148
SrejonKhan <srejonkhan9@gmail.com>
2024-05-13 11:28:45 +0600

    feat: add auth related files (WIP)

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,6 +1,10 @@
 import httpStatus from "http-status";
 
 const handleSignIn = async (req, res, next) => {
+  const payload = req.body 
+  const { email, password } = req.body;
+  
+
   res.status(httpStatus.OK).send({ msg: "Hello" });
 };
 
--- src/routers/v1/auth.ts
+++ src/routers/v1/auth.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { handleSignIn } from "../../controllers/authController";
+import { handleSignIn } from "../../controllers/auth.controller";
 
 const authRouter = express.Router();
 
