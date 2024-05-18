00ced0a5ce96758b00effcabfa4e79e007a09a9f
Srejon Khan <srejonkhan79@gmail.com>
2024-05-18 16:36:07 +0600

    feat(auth): update signin zod schema

--- src/schemas/auth.schema.ts
+++ src/schemas/auth.schema.ts
@@ -1,10 +1,8 @@
 import { z } from "zod";
 
 export const signInSchema = z.object({
-  email: z.string().refine((val) => val.trim().length > 0, {
-    message: "Email is required",
-  }),
-  password: z.string().refine((val) => val.trim().length >= 6, {
+  email: z.string().email({ message: "Invalid email address" }),
+  password: z.string().min(6, {
     message: "Password must be at least 6 characters",
   }),
 });
