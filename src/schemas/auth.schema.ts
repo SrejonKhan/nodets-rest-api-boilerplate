c4b837d3e35f412bac107237373e7971c0dfe618
Srejon Khan <srejonkhan79@gmail.com>
2024-05-18 16:37:01 +0600

    feat(auth): add signup zod schema

--- src/schemas/auth.schema.ts
+++ src/schemas/auth.schema.ts
@@ -6,3 +6,12 @@ export const signInSchema = z.object({
     message: "Password must be at least 6 characters",
   }),
 });
+
+export const signUpSchema = z.object({
+  email: z.string().email({ message: "Invalid email address" }),
+  password: z.string().min(6, {
+    message: "Password must be at least 6 characters",
+  }),
+  username: z.string().min(3, { message: "Username must be 3 or more characters long." }),
+  displayName: z.string().min(3, { message: "Display Name must be 3 or more characters long." }),
+});
