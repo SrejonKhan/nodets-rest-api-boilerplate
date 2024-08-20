70fd647aef77ca303aa2d895040a8ffed1c14dc4
Srejon Khan <srejonkhan79@gmail.com>
2024-08-20 13:24:36 +0600

    chore: update .env.example

--- src/config/base.config.ts
+++ src/config/base.config.ts
@@ -2,23 +2,23 @@ import { config as dotenvConfig } from "dotenv";
 dotenvConfig();
 
 const config = {
-  // GENERAL
+  // GENERAL APP CONFIG
   ENV: process.env.NODE_ENV,
   PORT: process.env.PORT,
 
-  // ASSYMMETRIC AUTH
+  // ASYMMETRIC AUTH CONFIG
   RSA_PRIVATE_KEY: Buffer.from(process.env.RSA_PRIVATE_KEY, "utf-8").toString(),
   RSA_PUBLIC_KEY: Buffer.from(process.env.RSA_PUBLIC_KEY, "utf-8").toString(),
   BCRYPT_SALT_ROUNDS: Number(process.env.BCRYPT_SALT_ROUNDS),
 
-  // AMQP
+  // AMQP CONFIG
   AMQP_URI: process.env.AMQP_URI,
 
-  // GOOGLE
+  // GCP CONFIG
   GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
   GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
 
-  // AWS S3
+  // AWS S3 CONFIG
   AWS_S3_ENDPOINT: process.env.AWS_S3_ENDPOINT,
   AWS_S3_REGION: process.env.AWS_S3_REGION,
   AWS_S3_ACCESS_KEY: process.env.AWS_S3_ACCESS_KEY,
